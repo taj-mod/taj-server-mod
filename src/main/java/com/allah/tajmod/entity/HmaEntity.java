f9026d1ff4d624d003342b9e214e8f7def60df35
cem <cemcem2003@gmail.com>
2021-12-07 18:10:01 +1100

    basic attack ai

--- src/main/java/com/allah/tajmod/entity/HmaEntity.java
+++ src/main/java/com/allah/tajmod/entity/HmaEntity.java
@@ -27,7 +27,6 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 import com.allah.tajmod.entity.goals.HmaAttackGoal;
-
 import java.util.Random;
 
 public class HmaEntity extends HostileEntity implements IAnimatable {
