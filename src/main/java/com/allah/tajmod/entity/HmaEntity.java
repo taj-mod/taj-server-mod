bb72e1f57c6bb87f474da017cd96ff37a3234c65
cem <cemcem2003@gmail.com>
2021-12-07 17:39:13 +1100

    basic attack ai

--- src/main/java/com/allah/tajmod/entity/HmaEntity.java
+++ src/main/java/com/allah/tajmod/entity/HmaEntity.java
@@ -4,9 +4,16 @@ import com.allah.tajmod.TajMod;
 import net.fabricmc.fabric.mixin.gamerule.GameRulesAccessor;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.ZombifiedPiglinEntity;
+import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.LightType;
 import net.minecraft.world.ServerWorldAccess;
@@ -19,6 +26,7 @@ import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
+import com.allah.tajmod.entity.goals.HmaAttackGoal;
 
 import java.util.Random;
 
@@ -31,11 +39,31 @@ public class HmaEntity extends HostileEntity implements IAnimatable {
         this.ignoreCameraFrustum = true;
     }
 
+    protected void initGoals() {
+        this.goalSelector.add(8, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.add(8, new LookAroundGoal(this));
+        this.initCustomGoals();
+    }
+
+    protected void initCustomGoals() {
+        this.goalSelector.add(2, new HmaAttackGoal(this, 1.0D, false));
+        this.goalSelector.add(7, new WanderAroundFarGoal(this, 1.0D));
+        this.targetSelector.add(1, (new RevengeGoal(this, new Class[0])).setGroupRevenge(new Class[]{ZombifiedPiglinEntity.class}));
+        this.targetSelector.add(2, new ActiveTargetGoal(this, PlayerEntity.class, true));
+        this.targetSelector.add(3, new ActiveTargetGoal(this, MerchantEntity.class, false));
+        this.targetSelector.add(3, new ActiveTargetGoal(this, IronGolemEntity.class, true));
+    }
+
+    public static DefaultAttributeContainer.Builder createHmaAttributes() {
+        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 60.0).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.5f).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 7.0).add(EntityAttributes.GENERIC_FOLLOW_RANGE, 64.0);
+    }
+
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event)
     {
         event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.bat.fly", true));
         return PlayState.CONTINUE;
     }
+
 
     @Override
     public void registerControllers(AnimationData data)
@@ -48,9 +76,7 @@ public class HmaEntity extends HostileEntity implements IAnimatable {
     {
         return this.factory;
     }
-    public static DefaultAttributeContainer.Builder createHmaAttributes() {
-        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 60.0).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3f).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 7.0).add(EntityAttributes.GENERIC_FOLLOW_RANGE, 64.0);
-    }
+
 
     @Override
     public boolean canSpawn(WorldAccess worldaccess, SpawnReason spawnReason) {
