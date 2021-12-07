5a70fe4889180bf0bb92b057cf77299f58ec77fd
sachk <sk@sachk.com>
2021-12-07 17:12:46 +1100

    Fix Hma sword tooltip

--- src/main/java/com/allah/tajmod/mixin/HeldItemTooltipMixin.java
+++ src/main/java/com/allah/tajmod/mixin/HeldItemTooltipMixin.java
@@ -2,6 +2,8 @@ package com.allah.tajmod.mixin;
 
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class HeldItemTooltipMixin {
     @Inject(method = "    renderHeldItemTooltip(Lnet/minecraft/client/util/math/MatrixStack;)V\n", at = @At("HEAD"), cancellable = true)
     private void injected(MatrixStack matrices, CallbackInfo ci) {
-        if (((InGameHud)(Object)this).currentStack.getName())
-        ci.cancel();
+        if (((InGameHud)(Object)this).currentStack.getName().toString().contains("hma_sword") || ((InGameHud)(Object)this).currentStack.getName().toString().contains("hm_sword")) {
+            ci.cancel();
+        }
     }
 }
