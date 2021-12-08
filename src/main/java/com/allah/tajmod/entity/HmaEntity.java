224817e4d276f0391abefcc6a282a981a4feeca4
cem <cemcem2003@gmail.com>
2021-12-08 12:45:47 +1100

    adds climbing

--- src/main/java/com/allah/tajmod/entity/HmaEntity.java
+++ src/main/java/com/allah/tajmod/entity/HmaEntity.java
@@ -5,10 +5,14 @@ import net.fabricmc.fabric.mixin.gamerule.GameRulesAccessor;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
+import net.minecraft.entity.ai.pathing.SpiderNavigation;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.mob.HostileEntity;
-import net.minecraft.entity.mob.ZombifiedPiglinEntity;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.mob.*;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -31,6 +35,22 @@ import java.util.Random;
 
 public class HmaEntity extends HostileEntity implements IAnimatable {
     private AnimationFactory factory = new AnimationFactory(this);
+    private static final TrackedData<Byte> HMA_FLAGS;
+
+
+
+
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.dataTracker.startTracking(HMA_FLAGS, (byte)0);
+    }
+    static {
+        HMA_FLAGS = DataTracker.registerData(HmaEntity.class, TrackedDataHandlerRegistry.BYTE);
+    }
+
+    public boolean isOnFire() {
+        return false;
+    }
 
     public HmaEntity(EntityType<? extends HostileEntity> type, World worldIn)
     {
@@ -51,17 +71,50 @@ public class HmaEntity extends HostileEntity implements IAnimatable {
         this.targetSelector.add(2, new ActiveTargetGoal(this, PlayerEntity.class, true));
         this.targetSelector.add(3, new ActiveTargetGoal(this, MerchantEntity.class, false));
         this.targetSelector.add(3, new ActiveTargetGoal(this, IronGolemEntity.class, true));
-        this.targetSelector.add(4, new ActiveTargetGoal(this, HostileEntity.class, true));
+        this.targetSelector.add(4, new ActiveTargetGoal(this, SkeletonEntity.class, true));
+        this.targetSelector.add(4, new ActiveTargetGoal(this, ZombieEntity.class, true));
+        this.targetSelector.add(4, new ActiveTargetGoal(this, CreeperEntity.class, true));
+        this.targetSelector.add(4, new ActiveTargetGoal(this, SpiderEntity.class, true));
     }
 
+    protected EntityNavigation createNavigation(World world) {
+        return new SpiderNavigation(this, world);
+    }
+
+
     public static DefaultAttributeContainer.Builder createHmaAttributes() {
-        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 60.0).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.5f).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 7.0).add(EntityAttributes.GENERIC_FOLLOW_RANGE, 64.0);
+        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 60.0).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.4f).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 4.0).add(EntityAttributes.GENERIC_FOLLOW_RANGE, 64.0);
     }
 
     protected boolean burnsInDaylight() {
         return true;
     }
 
+    public boolean isClimbing() {
+        return this.isClimbingWall();
+    }
+
+    public boolean isClimbingWall() {
+        return ((Byte)this.dataTracker.get(HMA_FLAGS) & 1) != 0;
+    }
+
+    public void setClimbingWall(boolean climbing) {
+        byte b = (Byte)this.dataTracker.get(HMA_FLAGS);
+        if (climbing) {
+            b = (byte)(b | 1);
+        } else {
+            b &= -2;
+        }
+
+        this.dataTracker.set(HMA_FLAGS, b);
+    }
+    public void tick() {
+        super.tick();
+        if (!this.world.isClient) {
+            this.setClimbingWall(this.horizontalCollision);
+        }
+
+    }
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event)
     {
