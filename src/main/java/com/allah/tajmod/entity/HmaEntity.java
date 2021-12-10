187fee446598e1df87975cf02b2672346c79a8af
sachk <sk@sachk.com>
2021-12-10 18:00:28 +1100

    Fix Hma attack animation

--- src/main/java/com/allah/tajmod/entity/HmaEntity.java
+++ src/main/java/com/allah/tajmod/entity/HmaEntity.java
@@ -19,9 +19,9 @@ import net.minecraft.entity.mob.ZombifiedPiglinEntity;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
-import net.minecraft.world.dimension.DimensionType;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -29,19 +29,18 @@ import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
-import com.allah.tajmod.entity.goals.HmaAttackGoal;
 
 public class HmaEntity extends HostileEntity implements IAnimatable {
     private AnimationFactory factory = new AnimationFactory(this);
     private static final TrackedData<Byte> HMA_FLAGS;
+    public static final TrackedData<Float> ATTACK_TICK_VISUAL = DataTracker.registerData(HmaEntity.class, TrackedDataHandlerRegistry.FLOAT);
 
-    private int attacktime = 0;
-
-
+    private float attackTick;
 
     protected void initDataTracker() {
         super.initDataTracker();
         this.dataTracker.startTracking(HMA_FLAGS, (byte)0);
+        dataTracker.startTracking(ATTACK_TICK_VISUAL, 0F);
     }
     static {
         HMA_FLAGS = DataTracker.registerData(HmaEntity.class, TrackedDataHandlerRegistry.BYTE);
@@ -68,7 +67,7 @@ public class HmaEntity extends HostileEntity implements IAnimatable {
     }
 
     protected void initCustomGoals() {
-        this.goalSelector.add(2, new HmaAttackGoal(this, 1.0D, false));
+        this.goalSelector.add(2, new AttackGoal());
         this.goalSelector.add(7, new WanderAroundFarGoal(this, 1.0D));
         this.targetSelector.add(1, (new RevengeGoal(this, new Class[0])).setGroupRevenge(new Class[]{ZombifiedPiglinEntity.class}));
         this.targetSelector.add(2, new ActiveTargetGoal(this, PlayerEntity.class, true));
@@ -86,7 +85,7 @@ public class HmaEntity extends HostileEntity implements IAnimatable {
 
 
     public static DefaultAttributeContainer.Builder createHmaAttributes() {
-        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 60.0).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3f).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 0.0).add(EntityAttributes.GENERIC_FOLLOW_RANGE, 150.0).add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE,1D);
+        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 40.0).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3f).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 0.0).add(EntityAttributes.GENERIC_FOLLOW_RANGE, 150.0).add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE,1D);
     }
 
 
@@ -109,6 +108,10 @@ public class HmaEntity extends HostileEntity implements IAnimatable {
         this.dataTracker.set(HMA_FLAGS, b);
     }
     public void tick() {
+        if (attackTick > 0F) {
+            attackTick = attackTick - 0.08F;
+            dataTracker.set(ATTACK_TICK_VISUAL, attackTick);
+        }
         super.tick();
         if (!this.world.isClient) {
             this.setClimbingWall(this.horizontalCollision);
@@ -124,45 +127,76 @@ public class HmaEntity extends HostileEntity implements IAnimatable {
             d = 999.0D;
         }
 
-        if (d < 70D && this.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED) < 0.5f) {
-            this.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).addPersistentModifier(new EntityAttributeModifier("Charge bonus", 0.2f, EntityAttributeModifier.Operation.ADDITION));
+        if (d < 70D && this.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED) < 0.4f) {
+            this.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).addPersistentModifier(new EntityAttributeModifier("Charge bonus", 0.15f, EntityAttributeModifier.Operation.ADDITION));
 
         } else if (d > 70D && this.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED) > 0.3f) {
-            this.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).addPersistentModifier(new EntityAttributeModifier("Charge bonus", -0.2f, EntityAttributeModifier.Operation.ADDITION));
+            this.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).addPersistentModifier(new EntityAttributeModifier("Charge bonus", -0.15f, EntityAttributeModifier.Operation.ADDITION));
         }
-        //System.out.println(this.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED));
-
-
     }
 
-    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event)
+    private <E extends IAnimatable> PlayState walk(AnimationEvent<E> event)
     {
-        if (event.getController().getCurrentAnimation() != null) {
-            if (event.getController().getCurrentAnimation().animationName == "hma attack") {
-                return PlayState.CONTINUE;
-            }
-        }
-        if (this.isAttacking()) {
-            System.out.println("attacking");
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("hma attack", false));
-            return PlayState.CONTINUE;
-        }
-        else if (this.prevX == this.getX() && this.prevY == this.getY() && this.prevZ == this.getZ()) {
-            //System.out.println("not walking");
+        if (this.prevX == this.getX() && this.prevY == this.getY())  {
             return PlayState.STOP;
         }
         else {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("hma walk", true));
-            //System.out.println("walking");
             return PlayState.CONTINUE;
         }
     }
 
+    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event)
+    {
+        float attackTick = this.getDataTracker().get(HmaEntity.ATTACK_TICK_VISUAL);
+        if (attackTick > 0F) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("hma attack", true));
+            return PlayState.CONTINUE;
+        }
+        return PlayState.STOP;
+    }
+
+    @Override
+    public void writeCustomDataToNbt(NbtCompound tag) {
+        super.writeCustomDataToNbt(tag);
+        tag.putFloat("AttackTick", this.attackTick);
+    }
+
+    @Override
+    public void readCustomDataFromNbt(NbtCompound tag) {
+        super.readCustomDataFromNbt(tag);
+        this.attackTick = tag.getFloat("AttackTick");
+    }
+
+    private class AttackGoal extends MeleeAttackGoal {
+        public AttackGoal() {
+            super(HmaEntity.this, 1.0D, true);
+        }
+
+        @Override
+        public double getSquaredMaxAttackDistance(LivingEntity entity) {
+            float f = HmaEntity.this.getWidth();
+            return (double) (f * f * 1.3D + entity.getWidth());
+        }
+
+        @Override
+        protected void attack(LivingEntity target, double squaredDistance) {
+            double number = this.getSquaredMaxAttackDistance(target);
+            System.out.println(attackTick);
+            if (squaredDistance <= number && attackTick <= 0F) {
+                //world.playSoundFromEntity(null, HmaEntity.this, SoundInit.PIGLINBEAST_CLUBSWING_EVENT, SoundCategory.HOSTILE, 1.0F, 1.0F);
+                this.resetCooldown();
+                this.mob.tryAttack(target);
+                attackTick = 1F;
+            }
+        }
+    }
 
     @Override
     public void registerControllers(AnimationData data)
     {
-        data.addAnimationController(new AnimationController(this, "controller", 0, this::predicate));
+        data.addAnimationController(new AnimationController(this, "walkcontroller", 0, this::walk));
+        data.addAnimationController(new AnimationController(this, "attackcontroller", 0, this::predicate));
     }
 
     @Override
@@ -174,16 +208,20 @@ public class HmaEntity extends HostileEntity implements IAnimatable {
 
     @Override
     public boolean canSpawn(WorldAccess worldaccess, SpawnReason spawnReason) {
+        if (world.getDimension().isPiglinSafe()) {
+            return false;
+        }
         if (world.getDimension().hasEnderDragonFight()) {
-            return true;
+            if (this.getPathfindingFavor(this.getBlockPos(), world) >= 0.0F) {
+                return true;
+            }
+            return false;
         }
         if (spawnReason == spawnReason.NATURAL) {
             if(world.getGameRules().getBoolean(TajMod.HMA_SPAWNING)) {
-            if (this.getPathfindingFavor(this.getBlockPos(), world) >= 0.0F) {
-                System.out.println(world.getTimeOfDay());
-                return (world.getTimeOfDay() > 12000);
-            }
-
+                if (this.getPathfindingFavor(this.getBlockPos(), world) >= 0.0F) {
+                    return (world.getTimeOfDay() > 12000);
+                }
             }
             return false;
         }
@@ -193,7 +231,9 @@ public class HmaEntity extends HostileEntity implements IAnimatable {
     @Override
     public boolean tryAttack(Entity target){
         this.setAttacking(true);
-        return target.damage(DamageSource.GENERIC, 4);
+        target.damage(DamageSource.GENERIC, 2);
+        return target.damage(DamageSource.mob(this), 4);
+
     }
 
     @Override
